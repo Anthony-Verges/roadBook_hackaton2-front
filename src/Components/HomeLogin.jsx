ead2a9ed67ad0a0ba0918294db3016423bae8080
Albin Marchand <albin.marchand@gmail.com>
2021-01-13 22:45:32 +0100

    Add CSS button and color

--- src/Components/HomeLogin.jsx
+++ src/Components/HomeLogin.jsx
@@ -8,11 +8,11 @@ const Login = () => {
       <Img src={logo} alt="" />
       <label>
         <Paragraphe>Username</Paragraphe>
-        <input type="text" />
+        <Input type="text" />
       </label>
       <label>
         <Paragraphe>Password</Paragraphe>
-        <input type="password" />
+        <Input type="password" />
       </label>
       <div>
         <Button type="submit">Submit</Button>
@@ -20,6 +20,17 @@ const Login = () => {
     </Form>
   );
 };
+
+const Img = styled.img`
+  width: 250px;
+`;
+
+const Input = styled.input`
+  border-radius: 50px;
+  border-style: none;
+  width: 200px;
+  height: 25px;
+`;
 
 const Form = styled.form`
   background-color: #5ad1d7;
@@ -35,16 +46,14 @@ const Button = styled.button`
   padding: 14px 40px;
   border-style: none;
   margin-top: 25px;
+  color: #5ad1d7;
+  font-size: 15px;
 `;
 
 const Paragraphe = styled.p`
   color: white;
   font-size: 20px;
   font-family: “Pragati Narrow”, sans-serif; ;
-`;
-
-const Img = styled.img`
-  width: 250px;
 `;
 
 export default Login;
