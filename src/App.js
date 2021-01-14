aa0b727c9a041923ea853648b7c1c52ca94af97e
dianecasanova <festusnova1989@gmail.com>
2021-01-14 11:21:54 +0100

    Layout done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,5 @@
-import Header from "./Components/Header/Header";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import "./App.css";
-import Footer from "./Components/Footer/Footer";
-
 function App() {
-  return (
-    <div>
-      <Header />
-      {/* <MapContainer
-        id="mapId"
-        // style={{ width: "100px", heigth: "100px" }}
-        center={[43.477, -1.565]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        /> */}
-      {/* <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker> */}
-      {/* </MapContainer> */}
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,13 @@
 import PropTypes from "prop-types";
 import { Col, Container, Row } from "reactstrap";
 import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
+// import Header from "./Components/Header/Header";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Container fluid>
-        <Header />
         <Row>
-          <Col md="2"></Col>
           <Col md="10">{children}</Col>
         </Row>
       </Container>
