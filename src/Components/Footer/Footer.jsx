14f7c7e0af22a49d097a8106068fc6e20332ee81
SALOMON VICTOR <victor-salomon@hotmail.fr>
2021-01-14 02:02:34 +0100

    added a footer and the header

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import logo from "../Images/logoOTRA_W.png";
+import styled from "styled-components";
+import logoFb from "./logoFacebook.png";
+import logoLkd from "./logoLinkedin.png";
+import logoInsta from "./logoInstagram.png";
+
+const Footer = () => {
+  return (
+    <div>
+      <Background>
+        <Container fluid>
+          <RowStyled>
+            <Col>
+              <Logo1
+                src={logo}
+                alt="TRAVELR your new travel partner all around the word"
+              />
+              <Paragraphe>
+                TRAVELR lets you keep track of all your favorite places around
+                the world, plan your trip easly, and find the best way to enjoy
+                your next trip!
+              </Paragraphe>
+            </Col>
+            <Col>
+              <List>CONTACT US</List>
+              <List>LEGAL INFORMATION</List>
+              <List>PRIVACY POLICY</List>
+              <List>MY ACCOUNT</List>
+            </Col>
+            <Col>
+              <RowStyled>
+                <Col>
+                  <Logo2 src={logoInsta} alt="" />
+                </Col>
+                <Col>
+                  <Logo2 src={logoFb} alt="" />
+                </Col>
+                <Col>
+                  <Logo2 src={logoLkd} alt="" />
+                </Col>
+              </RowStyled>
+            </Col>
+          </RowStyled>
+        </Container>
+      </Background>
+    </div>
+  );
+};
+
+const RowStyled = styled(Row)`
+  display: flex;
+`;
+
+const Background = styled.div`
+  background-color: #5ad1d7;
+  padding: 50px 0 50px 0;
+`;
+
+const List = styled.li`
+  padding-top: 20px;
+  list-style: none;
+  margin: 10px;
+  color: white;
+`;
+
+const Logo1 = styled.img`
+  margin-left: 20px;
+  width: 120px;
+`;
+
+const Logo2 = styled.img`
+  width: 30px;
+`;
+
+const Paragraphe = styled.p`
+  margin-left: 20px;
+  color: white;
+`;
+
+export default Footer;
---
+++ src/Components/Header/Burger/MenuItem.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import icon from "./ping.png";
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+export const MenuItem = ({ path, title }) => {
+  return (
+    <>
+      <motion.li
+        variants={variants}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        <img src={icon} />
+        <a className="styleMenuItems" href={path}>
+          <span>{title}</span>
+        </a>
+      </motion.li>
+    </>
+  );
+};
