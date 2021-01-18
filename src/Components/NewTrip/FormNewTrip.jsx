2d5900ca8ae4ca80eb9d1f412d6a2298ffc817e2
Albin Marchand <albin.marchand@gmail.com>
2021-01-18 10:53:57 +0100

    Updating files storage

--- src/Components/NewTrip/FormNewTrip.jsx
+++ src/Components/NewTrip/FormNewTrip.jsx
@@ -1,7 +1,7 @@
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { useState } from "react";
 import axios from "axios";
-import { API_URL } from "../../env";
+import { API_URL } from "../../../env";
 
 // const express = require("express");
 // const router = express.Router();
